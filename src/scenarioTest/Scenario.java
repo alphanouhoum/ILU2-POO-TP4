8c85cba48c3b59715d70662dab62c5cd992a33ec
alphanouhoum <alpha-nouhoum.toure@utoulouse.fr>
2026-04-16 19:57:47 +0200

    Classe anonyme

--- src/scenarioTest/Scenario.java
+++ src/scenarioTest/Scenario.java
@@ -1,61 +1,81 @@
 package scenarioTest;
 
+import java.util.Iterator;
+
+import javax.swing.text.StyledEditorKit.ItalicAction;
+
 import personnages.Gaulois;
-import produit.Produit;
 import produit.Poisson;
+import produit.Produit;
 import produit.Sanglier;
 import villagegaulois.Etal;
 import villagegaulois.IEtal;
 import villagegaulois.IVillage;
 
+
 public class Scenario {
 
 	public static void main(String[] args) {
 
-		// TODO Partie 4 : creer de la classe anonyme Village
 		IVillage village = new IVillage() {
-			
-			
+			private int nbEtals = 3;
+			private IEtal[] marche = new IEtal[nbEtals];
+			private int nbEtalAttribuer = 0;
+
 			@Override
-			public <P extends Produit> boolean installerVendeur(IEtal etal, Gaulois vendeur, P[] produit, int prix) {
-			
-				return false;
+			public <P extends Produit> boolean installerVendeur(Etal<P> etal, Gaulois vendeur, P[] produit, int prix) {
+				if (nbEtalAttribuer >= nbEtals) return false;
+
+				etal.installerVendeur(vendeur, produit, prix);
+				marche[nbEtalAttribuer++] = etal;
+				return true;
 			}
-			
+
 			@Override
 			public void acheterProduit(String produit, int quantiteSouhaitee) {
-				// TODO Auto-generated method stub
-				
+
+				int reste = quantiteSouhaitee;
+
+				for (int i = 0; i < nbEtalAttribuer && reste > 0; i++) {
+					int dispo = marche[i].contientProduit(produit, reste);
+
+					if (dispo > 0) {
+						int prix = marche[i].acheterProduit(dispo);
+						System.out.println("Etal " + (i + 1) + " : " + dispo + " achetés pour " + prix);
+						reste -= dispo;
+					}
+				}
+
+				System.out.println("Demandé : " + quantiteSouhaitee + ", acheté : " + (quantiteSouhaitee - reste));
 			}
-			
-			
-			
-			
+
+			@Override
+			public String toString() {
+				StringBuilder sb = new StringBuilder("=== Marché ===\n");
+				for (int i = 0; i < nbEtalAttribuer; i++) {
+					sb.append("Etal ").append(i + 1).append("\n");
+					sb.append(marche[i].etalEtal());
+				}
+				return sb.toString();
+			}
 		};
-		// fin
 
 		Gaulois ordralfabetix = new Gaulois("Ordralfabétix", 9);
 		Gaulois obelix = new Gaulois("Obélix", 20);
 		Gaulois asterix = new Gaulois("Astérix", 6);
 
-		IEtal etalSanglierObelix = new Etal<>();
-		IEtal etalSanglierAsterix = new Etal<>();
-		IEtal etalPoisson = new Etal<>();
+		// ✅ IMPORTANT : typage générique
+		Etal<Sanglier> e1 = new Etal<>();
+		Etal<Sanglier> e2 = new Etal<>();
+		Etal<Poisson> e3 = new Etal<>();
 
-		Sanglier sanglier1 = new Sanglier(200, obelix);
-		Sanglier sanglier2 = new Sanglier(150, obelix);
-		Sanglier sanglier3 = new Sanglier(100, asterix);
-		Sanglier sanglier4 = new Sanglier(50, asterix);
+		Sanglier[] s1 = { new Sanglier(200, obelix), new Sanglier(150, obelix) };
+		Sanglier[] s2 = { new Sanglier(100, asterix), new Sanglier(150, asterix) };
+		Poisson[] p = { new Poisson("lundi") };
 
-		Sanglier[] sangliersObelix = { sanglier1, sanglier2 };
-		Sanglier[] sangliersAsterix = { sanglier3, sanglier4 };
-
-		Poisson poisson1 = new Poisson("lundi");
-		Poisson[] poissons = { poisson1 };
-
-		village.installerVendeur(etalSanglierAsterix, asterix, sangliersAsterix, 10);
-		village.installerVendeur(etalSanglierObelix, obelix, sangliersObelix, 8);
-		village.installerVendeur(etalPoisson, ordralfabetix, poissons, 5);
+		village.installerVendeur(e1, obelix, s1, 8);
+		village.installerVendeur(e2, asterix, s2, 10);
+		village.installerVendeur(e3, ordralfabetix, p, 5);
 
 		System.out.println(village);
 
@@ -63,6 +83,4 @@ public class Scenario {
 
 		System.out.println(village);
 	}
-
 }
-
--- src/villagegaulois/IVillage.java
+++ src/villagegaulois/IVillage.java
@@ -4,7 +4,7 @@ import personnages.Gaulois;
 import produit.Produit;
 
 public interface IVillage {
-	<P extends Produit> boolean installerVendeur(IEtal etal, Gaulois vendeur, P[] produit, int prix);
+	<P extends Produit> boolean installerVendeur(Etal<P> etal, Gaulois vendeur, P[] produit, int prix);
 
 	void acheterProduit(String produit, int quantiteSouhaitee);
 }
